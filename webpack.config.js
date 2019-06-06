8f4adab5681a6453621acbe49dffa79738be5802
Phil Mayne <phil.mayne1@gmail.com>
2019-06-06 20:52:47 +0100

    Add content to cards

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = env => {
         },
         output: {
             filename: '[name].js',
-            path: path.resolve(__dirname, 'public')
+            path: path.resolve(__dirname, 'public'),
+            publicPath: path.resolve(__dirname, 'public')
         },
         module: {
             rules: [
