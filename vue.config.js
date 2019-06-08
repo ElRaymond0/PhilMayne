763b4351f3159497ce6df7397a0483f7f0ef796e
Phil Mayne <phil.mayne1@gmail.com>
2019-06-08 19:49:42 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/PhilMayne/'
-      : '/',
-    assetsDir: process.env.NODE_ENV === 'production'
-    ? '/PhilMayne/'
-    : '',
+      : '/'
   }
